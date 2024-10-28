9430fa547cd41fe1fdb5519657cdcd972dd4c977
Axel Sean Cahyono Putra <axelseancp@gmail.com>
2024-10-28 21:05:38 +0700

    pass say hello world test case

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -24,6 +24,14 @@ const createServer = async (container) => {
     },
   ]);
 
+  server.route({
+    method: "GET",
+    path: "/",
+    handler: () => ({
+      value: "Hello World!",
+    }),
+  });
+
   server.ext("onPreResponse", (request, h) => {
     const { response } = request;
 
