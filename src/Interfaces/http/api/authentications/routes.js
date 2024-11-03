6e216859a80d266b44d17e262c4015ebdd99a15c
Axel Sean Cahyono Putra <axelseancp@gmail.com>
2024-11-03 18:27:07 +0700

    add api documentation using swagger

--- src/Interfaces/http/api/authentications/routes.js
+++ src/Interfaces/http/api/authentications/routes.js
@@ -1,18 +1,73 @@
+const Joi = require("joi");
+
 const routes = (handler) => [
   {
     method: "POST",
     path: "/authentications",
     handler: handler.postAuthenticationHandler,
+    options: {
+      description: "POST authentications",
+      notes: "Test",
+      tags: ["api", "auth"],
+      validate: {
+        payload: Joi.object({
+          username: Joi.string(),
+          password: Joi.string(),
+        }).label("Post-authentications-payload"),
+      },
+      response: {
+        schema: Joi.object({
+          status: "success",
+          data: {
+            accessToken: Joi.string(),
+            refreshToken: Joi.string(),
+          },
+        }).label("Post-authentications-response"),
+      },
+    },
   },
   {
     method: "PUT",
     path: "/authentications",
     handler: handler.putAuthenticationHandler,
+    options: {
+      description: "PUT authentications",
+      notes: "Test",
+      tags: ["api", "auth"],
+      validate: {
+        payload: Joi.object({
+          refreshToken: Joi.string(),
+        }).label("Put-authentications-payload"),
+      },
+      response: {
+        schema: Joi.object({
+          status: "success",
+          data: {
+            accessToken: Joi.string(),
+          },
+        }).label("Put-authentications-response"),
+      },
+    },
   },
   {
     method: "DELETE",
     path: "/authentications",
     handler: handler.deleteAuthenticationHandler,
+    options: {
+      description: "DELETE authentications",
+      notes: "Test",
+      tags: ["api", "auth"],
+      validate: {
+        payload: Joi.object({
+          refreshToken: Joi.string(),
+        }).label("Delete-authentications-payload"),
+      },
+      response: {
+        schema: Joi.object({
+          status: "success",
+        }).label("Delete-authentications-response"),
+      },
+    },
   },
 ];
 
--- src/Interfaces/http/api/users/routes.js
+++ src/Interfaces/http/api/users/routes.js
@@ -3,6 +3,28 @@ const routes = (handler) => [
     method: "POST",
     path: "/users",
     handler: handler.postUserHandler,
+    options: {
+      description: "POST users",
+      notes: "Test",
+      tags: ["api", "auth"],
+      validate: {
+        payload: Joi.object({
+          username: Joi.string(),
+          fullname: Joi.string(),
+          password: Joi.string(),
+        }).label("Post-users-payload"),
+      },
+      response: {
+        schema: Joi.object({
+          status: "success",
+          data: {
+            id: Joi.string(),
+            username: Joi.string(),
+            fullname: Joi.string(),
+          },
+        }).label("Post-users-response"),
+      },
+    },
   },
 ];
 
